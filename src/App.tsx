48b0a080d2a580e85566ac31f684af33f20c0bc2
Евгений <zenja2625@gmail.com>
2022-06-14 18:03:41 +0300

    screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const ref = useRef<HTMLVideoElement | null>(null)
+    const canvasRef = useRef<HTMLCanvasElement | null>(null)
+    const [margin, setMargin] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+    const width = 200
+    const height = 200
+
+    useEffect(() => {
+        // alert('useEffect')
+        navigator.mediaDevices.enumerateDevices().then(function (devices) {
+            let id = ''
+
+            devices.forEach(function (device) {
+                if (device.kind === 'videoinput') {
+                    // alert(device.kind + ': ' + device.label + ' id = ' + device.deviceId)
+                    id = device.deviceId
+                }
+            })
+            navigator.mediaDevices.getUserMedia({ video: { deviceId: id } }).then(stream => {
+                const video = document.querySelector('video')
+                // включаем поток в магический URL
+                if (video !== null) video.srcObject = stream
+            })
+            // alert(id)
+        })
+    }, [])
+
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <div
+                    style={{
+                        position: 'relative',
+                        backgroundColor: 'red',
+                        width: `${width}px`,
+                        height: `${height}px`,
+                        overflow: 'hidden',
+                    }}
+                >
+                    <video
+                        ref={ref}
+                        onPlay={() => {
+                            if (ref.current) {
+                                const { height, width } = ref.current.getBoundingClientRect()
+
+                                setMargin({
+                                    x: width,
+                                    y: height,
+                                })
+                            }
+                        }}
+                        style={{
+                            position: 'absolute',
+                            top: `-${margin.y / 2 - height / 2}px`,
+                            left: `-${margin.x / 2 - width / 2}px`,
+                        }}
+                        autoPlay
+                    ></video>
+                </div>
+                <button
+                    onClick={() => {
+                        if (canvasRef.current && ref.current) {
+                            const canvas = canvasRef.current
+                            const video = ref.current
+
+                            canvas.width = video.videoWidth
+                            canvas.height = video.videoHeight
+                            canvas.getContext('2d')?.drawImage(video, 0, 0)
+                        }
+                    }}
+                >
+                    Screen
+                </button>
+                <canvas ref={canvasRef}></canvas>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
