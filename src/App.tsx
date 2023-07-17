a147643d3a61d98951c6841e033d42778ff3e6ae
Евгений Леонов <zenja2625@gmail.com>
2023-07-17 13:23:05 +0000

    On Click scan and if full match push to consultant

--- src/App.tsx
+++ src/App.tsx
@@ -14,233 +14,301 @@ import { log } from './log'
 type CameraRef = ElementRef<typeof Camera>
 
 function App() {
-    const [isButtonWait, setIsButtonWait] = useState(false)
+  const [isButtonWait, setIsButtonWait] = useState(false)
+  const stopLoopRef = useRef(false)
+  const [isRecord, setIsRecord] = useState(false)
 
-    const stopLoopRef = useRef(false)
-    const [isRecord, setIsRecord] = useState(false)
+  const { access, authorize, saveImageToGoogleDrive } = useGoogleApi()
 
-    const { access, authorize, saveImageToGoogleDrive } = useGoogleApi()
+  const [cvLoad, setCvLoad] = useState(false)
 
-    const [cvLoad, setCvLoad] = useState(false)
+  const [listOpen, setListOpen] = useState(false)
+  const [matchSponsors, setMatchSponsors] = useState<Array<Sponsor>>([])
+  const [sponsors, setSponsors] = useState<Array<Sponsor>>([])
+  const [ids, setIds] = useState<ID[]>([])
 
-    const [listOpen, setListOpen] = useState(false)
-    const [matchSponsors, setMatchSponsors] = useState<Array<Sponsor>>([])
-    const [sponsors, setSponsors] = useState<Array<Sponsor>>([])
-    const [ids, setIds] = useState<ID[]>([])
+  const [, setImages] = useState<{ name: string; value: Mat }[]>([])
 
-    const [, setImages] = useState<{ name: string; value: Mat }[]>([])
+  const { isLoaded, searchContours } = usePredictWorker()
+  // const { isLoad, isData, searchContours, setIds: setIds1 } = useModel()
 
-    const { isLoaded, searchContours } = usePredictWorker()
-    // const { isLoad, isData, searchContours, setIds: setIds1 } = useModel()
+  const cameraRef = useRef<CameraRef>(null)
 
-    const cameraRef = useRef<CameraRef>(null)
+  // useEffect(() => {
+  //     const foo = async () => {
+  //         const time = performance.now()
 
-    // useEffect(() => {
-    //     const foo = async () => {
-    //         const time = performance.now()
+  //         const src = await cameraRef.current?.getScreen()
 
-    //         const src = await cameraRef.current?.getScreen()
+  //         if (src) {
+  //             const matches = await searchContours(src, ids)
 
-    //         if (src) {
-    //             const matches = await searchContours(src, ids)
+  //             const sponsors: Array<Sponsor> = []
+  //             for (let i = 0; i < 3 && i < matches.length; i++) {
+  //                 const element = matches[i]
 
-    //             const sponsors: Array<Sponsor> = []
-    //             for (let i = 0; i < 3 && i < matches.length; i++) {
-    //                 const element = matches[i]
+  //                 sponsors.push({
+  //                     number: element.number.join(''),
+  //                     name: element.name.split(' ').pop() || element.name,
+  //                     phone: element.phone,
+  //                 })
+  //             }
 
-    //                 sponsors.push({
-    //                     number: element.number.join(''),
-    //                     name: element.name.split(' ').pop() || element.name,
-    //                     phone: element.phone,
-    //                 })
-    //             }
+  //             setMatchSponsors(sponsors.reverse())
 
-    //             setMatchSponsors(sponsors.reverse())
+  //             if (matches[0] && matches[0].number.join('') === matches[0].recognizeString) {
+  //                 setSponsors(prev => {
+  //                     const element = matches[0]
+  //                     const index = prev.findIndex(
+  //                         item => item.number === element.number.join('')
+  //                     )
 
-    //             if (matches[0] && matches[0].number.join('') === matches[0].recognizeString) {
-    //                 setSponsors(prev => {
-    //                     const element = matches[0]
-    //                     const index = prev.findIndex(
-    //                         item => item.number === element.number.join('')
-    //                     )
+  //                     if (index === -1) {
+  //                         return [
+  //                             ...prev,
+  //                             {
+  //                                 number: element.number.join(''),
+  //                                 name: element.name.split(' ').pop() || element.name,
+  //                                 phone: element.phone,
+  //                             },
+  //                         ]
+  //                     }
 
-    //                     if (index === -1) {
-    //                         return [
-    //                             ...prev,
-    //                             {
-    //                                 number: element.number.join(''),
-    //                                 name: element.name.split(' ').pop() || element.name,
-    //                                 phone: element.phone,
-    //                             },
-    //                         ]
-    //                     }
+  //                     return prev
+  //                 })
+  //             }
+  //         }
 
-    //                     return prev
-    //                 })
-    //             }
-    //         }
+  //         await log(Math.floor(performance.now() - time) + 'ms ')
+  //         // saveImageToGoogleDrive(src)
+  //         //!!!!!!!!!!!!!!!!!!!
+  //     }
 
-    //         await log(Math.floor(performance.now() - time) + 'ms ')
-    //         // saveImageToGoogleDrive(src)
-    //         //!!!!!!!!!!!!!!!!!!!
-    //     }
+  //     if (isRecord) {
+  //         foo()
+  //     }
+  // }, [isRecord])
 
-    //     if (isRecord) {
-    //         foo()
-    //     }
-    // }, [isRecord])
+  useEffect(() => {
+    const data = window.localStorage.getItem('data')
 
-    useEffect(() => {
-        const data = window.localStorage.getItem('data')
-
-        if (data) {
-            setIds(JSON.parse(data))
-        }
-        cv.onRuntimeInitialized = () => {
-            setCvLoad(true)
-        }
-    }, [])
-
-    if (!ids.length) {
-        return (
-            <div className='App'>
-                <header className='App-header' style={{ justifyContent: 'center' }}>
-                    <input
-                        type='file'
-                        onChange={e => {
-                            const fileReader = new FileReader()
-                            if (e.target.files) {
-                                fileReader.readAsText(e.target.files[0], 'UTF-8')
-                                fileReader.onload = e => {
-                                    if (e.target?.result) {
-                                        const data = (
-                                            JSON.parse(e.target.result.toString()) as any[]
-                                        ).map(item => ({
-                                            ...item,
-                                            number: (item.number as string)
-                                                .split('')
-                                                .map(n => parseInt(n)),
-                                        }))
-
-                                        setIds(data)
-                                        window.localStorage.setItem('data', JSON.stringify(data))
-                                    }
-                                }
-                            }
-                        }}
-                    />
-                </header>
-            </div>
-        )
+    if (data) {
+      setIds(JSON.parse(data))
     }
+    cv.onRuntimeInitialized = () => {
+      setCvLoad(true)
+    }
+  }, [])
 
+  if (!ids.length) {
     return (
-        <>
-            <canvas
-                id='show'
-                style={{
-                    position: 'absolute',
-                    top: '205px',
-                    backgroundColor: 'red',
-                    display: 'none',
-                }}
-            ></canvas>
+      <div className="App">
+        <header className="App-header" style={{ justifyContent: 'center' }}>
+          <input
+            type="file"
+            onChange={(e) => {
+              const fileReader = new FileReader()
+              if (e.target.files) {
+                fileReader.readAsText(e.target.files[0], 'UTF-8')
+                fileReader.onload = (e) => {
+                  if (e.target?.result) {
+                    const data = (
+                      JSON.parse(e.target.result.toString()) as any[]
+                    ).map((item) => ({
+                      ...item,
+                      number: (item.number as string)
+                        .split('')
+                        .map((n) => parseInt(n)),
+                    }))
 
-            <div className='wrapper'>
-                <Camera ref={cameraRef} height={200} width={200} />
-                <div
-                    onClick={() => authorize()}
-                    className={'list-button google-button' + (access ? ' google-button-auth' : '')}
-                >
-                    G
-                </div>
-                <div className='card-wrapper'>
-                    <div className='list-button' onClick={() => setListOpen(true)}>
-                        Список
-                    </div>
-                    <div className='card-list-wrapper'>
-                        <div className='card-list'>
-                            {sponsors
-                                .filter((_, index) => index > sponsors.length - 1 - 3)
-                                .map(({ number: code, name, phone }) => (
-                                    <Card key={code} number={code} name={name} phone={phone} />
-                                ))}
-                        </div>
-                        <div className='card-list'>
-                            {matchSponsors.map(({ number: code, name, phone }) => {
-                                const isSelected =
-                                    sponsors.findIndex(sponsor => sponsor.number === code) !== -1
-
-                                return (
-                                    <Card
-                                        isSelected={isSelected}
-                                        setSponsor={sponsor => {
-                                            if (!isSelected) setSponsors(prev => [...prev, sponsor])
-                                        }}
-                                        key={code}
-                                        number={code}
-                                        name={name}
-                                        phone={phone}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </div>
-                    {sponsors.length}
-                </div>
-                <div className='button-wrapper'>
-                    <div
-                        onClick={async () => {
-                            if (!isButtonWait) {
-                                setIsButtonWait(true)
-
-                                await new Promise(r => setTimeout(r, 2000))
-
-                                setIsRecord(prev => !prev)
-                                setIsButtonWait(false)
-                            }
-                            // if (!cvLoad || !isLoaded) return
-                        }}
-                        className={`button ${isRecord ? 'button-press' : ''}`}
-                    >
-                        <div className='button-center'></div>
-                    </div>
-                </div>
-            </div>
-            {listOpen && (
-                <SeletedSponsorList
-                    clear={() => setSponsors([])}
-                    removeSponsor={sponsorCode =>
-                        setSponsors(prev => prev.filter(sponsor => sponsor.number !== sponsorCode))
-                    }
-                    sponsors={sponsors}
-                    close={() => setListOpen(false)}
-                />
-            )}
-            <div
-                style={{
-                    position: 'absolute',
-                    width: '10px',
-                    height: '10px',
-                    backgroundColor: !cvLoad || !isLoaded ? 'red' : 'greenyellow',
-                    left: 10,
-                    bottom: 10,
-                    zIndex: 1000,
-                }}
-            ></div>
-            {isDebug && (
-                <div
-                    id='log'
-                    style={{
-                        position: 'absolute',
-                        right: 0,
-                        top: 0,
-                        color: 'white',
-                    }}
-                ></div>
-            )}
-        </>
+                    setIds(data)
+                    window.localStorage.setItem('data', JSON.stringify(data))
+                  }
+                }
+              }
+            }}
+          />
+        </header>
+      </div>
     )
+  }
+
+  return (
+    <>
+      <canvas
+        id="show"
+        style={{
+          position: 'absolute',
+          top: '205px',
+          backgroundColor: 'red',
+          display: 'none',
+        }}
+      ></canvas>
+
+      <div className="wrapper">
+        <Camera ref={cameraRef} height={200} width={200} />
+        <div
+          onClick={() => authorize()}
+          className={
+            'list-button google-button' + (access ? ' google-button-auth' : '')
+          }
+        >
+          G
+        </div>
+        <div className="card-wrapper">
+          <div className="list-button" onClick={() => setListOpen(true)}>
+            Список
+          </div>
+          <div className="card-list-wrapper">
+            <div className="card-list">
+              {sponsors
+                .filter((_, index) => index > sponsors.length - 1 - 3)
+                .map(({ number: code, name, phone }) => (
+                  <Card key={code} number={code} name={name} phone={phone} />
+                ))}
+            </div>
+            <div className="card-list">
+              {matchSponsors.map(({ number: code, name, phone }) => {
+                const isSelected =
+                  sponsors.findIndex((sponsor) => sponsor.number === code) !==
+                  -1
+
+                return (
+                  <Card
+                    isSelected={isSelected}
+                    setSponsor={(sponsor) => {
+                      if (!isSelected) setSponsors((prev) => [...prev, sponsor])
+                    }}
+                    key={code}
+                    number={code}
+                    name={name}
+                    phone={phone}
+                  />
+                )
+              })}
+            </div>
+          </div>
+          {sponsors.length}
+        </div>
+        <div className="button-wrapper">
+          <div
+            onClick={async () => {
+              if (!isRecord) {
+                setIsRecord(true)
+
+                await new Promise<void>(async (r) => {
+                  while (true) {
+                    //!!!!!!!!!!!!!!!!!!!!
+
+                    const time = performance.now()
+
+                    const src = await cameraRef.current?.getScreen()
+
+                    if (src) {
+                      const matches = await searchContours(src, ids)
+
+                      const sponsors: Array<Sponsor> = []
+                      for (let i = 0; i < 3 && i < matches.length; i++) {
+                        const element = matches[i]
+
+                        sponsors.push({
+                          number: element.number.join(''),
+                          name: element.name.split(' ').pop() || element.name,
+                          phone: element.phone,
+                        })
+                      }
+
+                      setMatchSponsors(sponsors.reverse())
+
+                      if (
+                        matches[0] &&
+                        matches[0].number.join('') ===
+                          matches[0].recognizeString
+                      ) {
+                        setSponsors((prev) => {
+                          const element = matches[0]
+                          const index = prev.findIndex(
+                            (item) => item.number === element.number.join('')
+                          )
+
+                          if (index === -1) {
+                            return [
+                              ...prev,
+                              {
+                                number: element.number.join(''),
+                                name:
+                                  element.name.split(' ').pop() || element.name,
+                                phone: element.phone,
+                              },
+                            ]
+                          }
+
+                          return prev
+                        })
+                      }
+                    }
+
+                    await log(Math.floor(performance.now() - time) + 'ms ')
+
+                    //!!!!!!
+
+                    if (stopLoopRef.current) {
+                      break
+                    }
+                  }
+
+                  r()
+                })
+
+                setIsRecord(false)
+                stopLoopRef.current = false
+              } else {
+                stopLoopRef.current = true
+              }
+              // if (!cvLoad || !isLoaded) return
+            }}
+            className={`button ${isRecord ? 'button-press' : ''}`}
+          >
+            <div className="button-center"></div>
+          </div>
+        </div>
+      </div>
+      {listOpen && (
+        <SeletedSponsorList
+          clear={() => setSponsors([])}
+          removeSponsor={(sponsorCode) =>
+            setSponsors((prev) =>
+              prev.filter((sponsor) => sponsor.number !== sponsorCode)
+            )
+          }
+          sponsors={sponsors}
+          close={() => setListOpen(false)}
+        />
+      )}
+      <div
+        style={{
+          position: 'absolute',
+          width: '10px',
+          height: '10px',
+          backgroundColor: !cvLoad || !isLoaded ? 'red' : 'greenyellow',
+          left: 10,
+          bottom: 10,
+          zIndex: 1000,
+        }}
+      ></div>
+      {isDebug && (
+        <div
+          id="log"
+          style={{
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            color: 'white',
+          }}
+        ></div>
+      )}
+    </>
+  )
 }
 
 export default App
--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,12 +1,12 @@
 import cv, { Mat } from 'opencv-ts'
 import {
-    forwardRef,
-    ForwardRefRenderFunction,
-    useCallback,
-    useEffect,
-    useImperativeHandle,
-    useRef,
-    useState,
+  forwardRef,
+  ForwardRefRenderFunction,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
 } from 'react'
 import refresh from './refresh.svg'
 import { Coors } from './types'
@@ -14,117 +14,122 @@ import { Coors } from './types'
 type Size = { width: number; height: number }
 type CameraRef = { getScreen: () => Promise<Mat | null> }
 
-const CameraFn: ForwardRefRenderFunction<CameraRef, Size> = ({ height, width }, ref) => {
-    const [loadCamera, setLoadCamera] = useState(false)
-    const [showRefresh, setShowRefresh] = useState(true)
-    const [offset, setOffset] = useState<Coors>({ left: 0, top: 0 })
-    const videoRef = useRef<HTMLVideoElement | null>(null)
+const CameraFn: ForwardRefRenderFunction<CameraRef, Size> = (
+  { height, width },
+  ref
+) => {
+  const [loadCamera, setLoadCamera] = useState(false)
+  const [showRefresh, setShowRefresh] = useState(true)
+  const [offset, setOffset] = useState<Coors>({ left: 0, top: 0 })
+  const videoRef = useRef<HTMLVideoElement | null>(null)
 
-    const startStream = useCallback(async () => {
-        setLoadCamera(true)
+  const startStream = useCallback(async () => {
+    setLoadCamera(true)
 
-        const devices = await navigator.mediaDevices.enumerateDevices()
-        const videoDevices = devices.filter(devise => devise.kind === 'videoinput')
-        const deviceId = videoDevices[videoDevices.length - 1]?.deviceId || null
-
-        if (deviceId) {
-            const stream = await navigator.mediaDevices.getUserMedia({
-                video: { deviceId, width: 1280, height: 720 },
-            })
-            if (videoRef.current !== null) {
-                setShowRefresh(false)
-                videoRef.current.srcObject = stream
-            }
-        }
-
-        setLoadCamera(false)
-    }, [])
-
-    useEffect(() => {
-        startStream()
-    }, [startStream])
-
-    useImperativeHandle(ref, () => ({
-        getScreen: async () => {
-            const video = videoRef.current
-
-            if (!video) return null
-
-            const bitmap = await createImageBitmap(video)
-            const c1 = document.getElementById('show') as HTMLCanvasElement
-            c1.height = 200
-            c1.width = 200
-            const ctx = c1.getContext('2d')
-            ctx?.drawImage(bitmap, offset.left, offset.top, 200, 200, 0, 0, 200, 200)
-            bitmap.close()
-
-            const img = cv.imread('show')
-
-            return img
-        },
-    }))
-
-    return (
-        <div
-            style={{
-                position: 'relative',
-                overflow: 'hidden',
-                height: `${height}px`,
-                width: `${width}px`,
-            }}
-        >
-            {showRefresh && (
-                <div
-                    onTouchStart={async () => {
-                        startStream()
-                    }}
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        fontSize: '40px',
-                        position: 'absolute',
-                        zIndex: 1,
-                        backgroundColor: 'black',
-                    }}
-                >
-                    <img
-                        src={refresh}
-                        className={'refresh' + (loadCamera ? ' refresh-load' : '')}
-                        draggable='false'
-                        alt=''
-                    />
-                </div>
-            )}
-            <video
-                ref={videoRef}
-                onPlay={e => {
-                    const { videoHeight, videoWidth } = e.currentTarget
-
-                    setOffset({
-                        left: videoWidth / 2 - height / 2,
-                        top: videoHeight / 2 - width / 2,
-                    })
-                }}
-                style={{
-                    position: 'absolute',
-                    top: `-${offset.top}px`,
-                    left: `-${offset.left}px`,
-                }}
-                // autoPlay
-            ></video>
-            <canvas
-                id='showMatches'
-                style={{
-                    position: 'absolute',
-                    height: '100%',
-                    // backgroundColor: 'yellow',
-                }}
-            ></canvas>
-        </div>
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const videoDevices = devices.filter(
+      (devise) => devise.kind === 'videoinput'
     )
+    const deviceId = videoDevices[videoDevices.length - 1]?.deviceId || null
+
+    if (deviceId) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId, width: 1280, height: 720 },
+      })
+      if (videoRef.current !== null) {
+        setShowRefresh(false)
+        videoRef.current.srcObject = stream
+      }
+    }
+
+    setLoadCamera(false)
+  }, [])
+
+  useEffect(() => {
+    startStream()
+  }, [startStream])
+
+  useImperativeHandle(ref, () => ({
+    getScreen: async () => {
+      const video = videoRef.current
+
+      if (!video) return null
+
+      const bitmap = await createImageBitmap(video)
+      const c1 = document.getElementById('show') as HTMLCanvasElement
+      c1.height = 200
+      c1.width = 200
+      const ctx = c1.getContext('2d')
+      ctx?.drawImage(bitmap, offset.left, offset.top, 200, 200, 0, 0, 200, 200)
+      bitmap.close()
+
+      const img = cv.imread('show')
+
+      return img
+    },
+  }))
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        overflow: 'hidden',
+        height: `${height}px`,
+        width: `${width}px`,
+      }}
+    >
+      {showRefresh && (
+        <div
+          onTouchStart={async () => {
+            startStream()
+          }}
+          style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '40px',
+            position: 'absolute',
+            zIndex: 1,
+            backgroundColor: 'black',
+          }}
+        >
+          <img
+            src={refresh}
+            className={'refresh' + (loadCamera ? ' refresh-load' : '')}
+            draggable="false"
+            alt=""
+          />
+        </div>
+      )}
+      <video
+        ref={videoRef}
+        onPlay={(e) => {
+          const { videoHeight, videoWidth } = e.currentTarget
+
+          setOffset({
+            left: videoWidth / 2 - height / 2,
+            top: videoHeight / 2 - width / 2,
+          })
+        }}
+        style={{
+          position: 'absolute',
+          top: `-${offset.top}px`,
+          left: `-${offset.left}px`,
+        }}
+        autoPlay
+      ></video>
+      <canvas
+        id="showMatches"
+        style={{
+          position: 'absolute',
+          height: '100%',
+          // backgroundColor: 'yellow',
+        }}
+      ></canvas>
+    </div>
+  )
 }
 
 export const Camera = forwardRef(CameraFn)
